4a6aa0a04eb3086836437e1bab6abfe72f8bd119
alisonh29 <alisonhernandez@triton.edu>
2025-12-11 19:49:17 -0600

    Add files via upload

---
+++ PS1P3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+string fname, lname;
+int x,y;
+int sum;
+int holdscore;
+int a,b,c;
+
+int main() {
+   
+ /*   cout << "Enter first name";
+    cin >> fname;
+    cout << "Enter last name";
+    cin >> lname;
+    cout << "Hello " << fname << " " << lname << endl;
+    cout << "Type a number:" ; // Type a number and press enter
+    cin >> x; // Get your user input from the keyboard
+    cout << "Type another number:" ; // Type a number and press enter
+    cin >> y; // Get your user input from the keyboard
+    sum = x+y;
+    cout << "The sum of "  << x << " and " << y << " is:" << x+y << endl;
+    */
+    cout << "Enter last name: ";
+    cin >> lname;
+    cout << "Enter score: ";
+    cin >> holdscore;
+    cout << lname << " has a score of " << holdscore << endl;
+    
+    /*
+    cout << "The sum of " << x << "and" << y << "is:" << x+y << endl;
+    cout << "The product of "  << x << " and " << y << " is:" << x*y << endl;
+    cout << "The difference of "  << x << " and " << y << " is:" << x - y << endl;
+
+    cout << "Type a number:" ; // Type a number and press enter
+    cin >> a; // Get your user input from the keyboard
+    cout << "Type another number:" ; // Type a number and press enter
+    cin >> b; // Get your user input from the keyboard
+    cout << "Type an additional number:" ; // Type a number and press enter
+    cin >> c; // Get your user input from the keyboard
+    cout << " The sum of "  << a << " and " << b << "multiplied by " << c << " is: " << (a+b)*c << endl;*/
+    return 0;
+}
---
+++ PS1P4.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+string fname, lname;
+int x,y;
+int sum;
+int holdscore;
+int n1,n2,n3;
+
+int main() {
+   
+ /*   cout << "Enter first name";
+    cin >> fname;
+    cout << "Enter last name";
+    cin >> lname;
+    cout << "Hello " << fname << " " << lname << endl;
+
+    cout << "Type a number:" ; // Type a number and press enter
+    cin >> x; // Get your user input from the keyboard
+    cout << "Type another number:" ; // Type a number and press enter
+    cin >> y; // Get your user input from the keyboard
+    sum = x+y;
+    cout << "The sum of "  << x << " and " << y << " is:" << x+y << endl;
+    
+    cout << "Enter last name:";
+    cin >> lname;
+    cout << "Enter score";
+    cin >> holdscore;
+    cout << lname << " has a score of " << holdscore << endl;
+  
+ 
+    
+    cout << "Type a number:" ; // Type a number and press enter
+    cin >> x; // Get your user input from the keyboard
+    cout << "Type another number:" ; // Type a number and press enter
+    cin >> y; // Get your user input from the keyboard
+    
+    cout << "The sum of " << x << "and" << y << "is:" << x+y << endl;
+    cout << "The product of "  << x << " and " << y << " is:" << x*y << endl;
+    cout << "The difference of "  << x << " and " << y << " is:" << x - y << endl;
+*/
+    cout << "Type a number:" ; // Type a number and press enter
+    cin >> n1; // Get your user input from the keyboard
+    cout << "Type another number:" ; // Type a number and press enter
+    cin >> n2; // Get your user input from the keyboard
+    cout << "Type an additional number:" ; // Type a number and press enter
+    cin >> n3; // Get your user input from the keyboard
+    cout << " The sum of "  << n1 << " and " << n2 << " multiplied by " << n3 << " is: " << (n1+n2)*n3 << endl;
+    return 0;
+}
